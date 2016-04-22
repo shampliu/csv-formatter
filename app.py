9e0323cbe79d032bfec60e51f5c24a2a24ce7d37
Chang Liu <shampliu@gmail.com>
2016-04-22 01:15:24 -0700

    dictreader

--- app.py
+++ app.py
@@ -3,25 +3,38 @@ import csv
 r1 = csv.reader(open('sanders.csv', 'rb'))
 r1.next()	# skip first line
 
-r2 = csv.reader(open('clinton.csv', 'rb'))
-r2.next()
+# r2 = csv.reader(open('clinton.csv', 'rb'))
+# r2.next()
 
-writer = csv.writer(open('result.csv', 'wb'))
+csvfile = open('result.csv', 'wb')
+writer = csv.writer(csvfile)
 
 for row in r1:
 	writer.writerow(row)
 
-for row in r2:
-	writer.writerow(row)
+# for row in r2:
+# 	writer.writerow(row)
 
-# d = {}
-# d['column1name'] = []
-# d['column2name'] = []
-# d['column3name'] = []
+csvfile.close()
+csvfile = open('result.csv', 'rb')
+jsonfile = open('result.json', 'wb')
 
-# dictReader = csv.DictReader(open('sanders.csv', 'rb'), fieldnames = ['column1name', 'column2name', 'column3name'], delimiter = ',', quotechar = '"')
+d = {}
+d['dem'] = {}
+d['rep'] = {}
 
-# for row in dictReader:
-#     for key in row:
-#         d[key].append(row[key])
-#         print "%s : %s " % (key, row[key])
+fieldnames = ["cmte_id","cand_id","cand_nm","contbr_nm","contbr_city","contbr_st","contbr_zip","contbr_employer","contbr_occupation","contb_receipt_amt","contb_receipt_dt","receipt_desc","memo_cd","memo_text","form_tp","file_num","tran_id","election_tp"]
+
+
+
+# dictReader = csv.DictReader(csvfile, fieldnames, delimiter = ',', quotechar = '"')
+dictReader = csv.DictReader(csvfile, fieldnames, delimiter = ',', quotechar = '"')
+
+curr_name = 0; 
+
+for row in dictReader:
+	print row["cand_nm"]
+    # for key in row:
+    # 	print key
+        # d[key].append(row[key])
+        # print "%s : %s " % (key, row[key])
