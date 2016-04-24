41deb34990c7e76d17768959cea424cb1fc99fab
Chang Liu <shampliu@gmail.com>
2016-04-23 22:57:24 -0700

    formatted json better for d3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import re
 
 r1 = csv.reader(open('donations.csv', 'rb'))
 r1.next()	# skip first line
@@ -14,133 +15,172 @@ csvfile.close()
 csvfile = open('result.csv', 'rb')
 jsonfile = open('result.json', 'wb')
 
-d = {
-	'dem' : {
-		'Sanders' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Clinton' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Lessig' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		"O'Malley" : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Webb' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		}
+d = [
+	{
+		"name" : "Sanders",
+		"party" : "dem",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
 	},
-	'rep' : { 
-		'Carson' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Cruz' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Kasich' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Bush' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Perry' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Fiorina' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Rubio' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Paul' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Trump' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		},
-		'Graham' : {
-			"total" : 0,
-			"colleges" : { },
-			"locations" : { }
-		}
-
+	{
+		"name" : "Clinton",
+		"party" : "dem",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Lessig",
+		"party" : "dem",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "O'Malley",
+		"party" : "dem",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Webb",
+		"party" : "dem",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Carson",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Cruz",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Kasich",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Bush",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Perry",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Fiorina",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Rubio",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Paul",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Trump",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
+	},
+	{
+		"name" : "Graham",
+		"party" : "rep",
+		"total" : 0,
+		"colleges_total" : 0,
+		"colleges" : [],
+		"locations" : []
 	}
-	
-}
+]
 
 
 fieldnames = ["cmte_id","cand_id","cand_nm","contbr_nm","contbr_city","contbr_st","contbr_zip","contbr_employer","contbr_occupation","contb_receipt_amt","contb_receipt_dt","receipt_desc","memo_cd","memo_text","form_tp","file_num","tran_id","election_tp"]
 
 name_hash = {
-	"Sanders, Bernard" 					: ('dem', 'Sanders'),
-	"Clinton, Hillary Rodham" 			: ('dem', 'Clinton'),
-	"Cruz, Rafael Edward 'Ted'" 		: ('rep', 'Cruz'),
-	"Kasich, John R." 					: ('rep', 'Kasich'),
-	"Carson, Benjamin S." 				: ('rep', 'Carson'),
-	"Webb, James Henry Jr." 			: ('dem', 'Webb'),
-	"O'Malley, Martin Joseph" 			: ('dem', "O'Malley"),
-	"Lessig, Lawrence" 					: ('dem', "Lessig"),
-	"Bush, Jeb" 						: ('rep', "Bush"),
-	"Rubio, Marco" 						: ('rep', "Rubio"),
-	"Fiorina, Carly" 					: ('rep', "Fiorina"),
-	"Paul, Rand" 						: ('rep', "Paul"),
-	"Graham, Lindsey O." 				: ('rep', "Graham"),
-	"Trump, Donald J." 					: ('rep', "Trump"),
-	"Perry, James R. (Rick)" 			: ('rep', "Perry")
+	"Sanders, Bernard" 							: ('Sanders', 0),
+	"Clinton, Hillary Rodham" 			: ('Clinton', 1),
+	"Cruz, Rafael Edward 'Ted'" 		: ('Cruz', 6),
+	"Kasich, John R." 							: ('Kasich', 7),
+	"Carson, Benjamin S." 					: ('Carson', 5),
+	"Webb, James Henry Jr." 				: ('Webb', 4),
+	"O'Malley, Martin Joseph" 			: ("O'Malley", 3),
+	"Lessig, Lawrence" 							: ("Lessig", 2),
+	"Bush, Jeb" 										: ("Bush", 8),
+	"Rubio, Marco" 									: ("Rubio", 11),
+	"Fiorina, Carly" 								: ("Fiorina", 10),
+	"Paul, Rand" 										: ("Paul", 12),
+	"Graham, Lindsey O." 						: ("Graham", 14),
+	"Trump, Donald J." 							: ("Trump", 13),
+	"Perry, James R. (Rick)" 				: ("Perry", 9)
 }
 
 def format_name(n):
 	return name_hash.get(n)		# returns None on other name
 
 college_hash = {
-	"UCLA" 											: "ucla", 
+	"UCLA" 																			: "ucla", 
 	"UNIVERSITY OF CALIFORNIA, LOS ANGELES" 		: "ucla",
-	"UCB" 											: "ucb",
-	"UNIVERSITY OF CALIFORNIA, BERKELEY" 			: "ucb",
-	"UCSB" 											: "ucsb",
-	"UNIVERSITY OF CALIFORNIA, SANTA BARBARA" 		: "ucsb",
-	"UCSC" 											: "ucsc",
+	"UCB" 																			: "ucb",
+	"UNIVERSITY OF CALIFORNIA, BERKELEY" 				: "ucb",
+	"UCSB" 																			: "ucsb",
+	"UNIVERSITY OF CALIFORNIA, SANTA BARBARA" 	: "ucsb",
+	"UCSC" 																			: "ucsc",
 	"UNIVERSITY OF CALIFORNIA, SANTA CRUZ" 			: "ucsc",
-	"UCR" 											: "ucr",
+	"UCR" 																			: "ucr",
 	"UNIVERSITY OF CALIFORNIA, RIVERSIDE" 			: "ucr",
-	"UCSD" 											: "ucsd",
+	"UCSD" 																			: "ucsd",
 	"UNIVERSITY OF CALIFORNIA, SAN DIEGO" 			: "ucsd",
-	"UCM" 											: "ucm",
-	"UNIVERSITY OF CALIFORNIA, MERCED" 				: "ucm",
-	"UCSF" 											: "ucsf",
-	"UNIVERSITY OF CALIFORNIA, SAN FRANCISCO" 		: "ucsf",
-	"UCD" 											: "ucd",
-	"UNIVERSITY OF CALIFORNIA, DAVIS" 				: "ucd"
+	"UCM" 																			: "ucm",
+	"UNIVERSITY OF CALIFORNIA, MERCED" 					: "ucm",
+	"UCSF" 																			: "ucsf",
+	"UNIVERSITY OF CALIFORNIA, SAN FRANCISCO" 	: "ucsf",
+	"UCD" 																			: "ucd",
+	"UNIVERSITY OF CALIFORNIA, DAVIS" 					: "ucd"
 }
 
 def format_college(c):
@@ -154,6 +194,10 @@ job_hash = {
 }
 
 def format_job(j):
+	# student = re.compile('[\w]*STUDENT[\w]*')
+	# if student.match(j):
+	# 	return "undergraduate"
+
 	return job_hash.get(j)
 
 
@@ -162,59 +206,76 @@ dict_reader = csv.DictReader(csvfile, fieldnames, delimiter = ',', quotechar = '
 for row in dict_reader:
 	t = format_name( row["cand_nm"] )
 	if t is None:
-		continue 
-	cand = d[t[0]][t[1]]
+		print row["cand_nm"]
+		continue
+	ind =  t[1]
+	cand = d[ind]
+
 
 	amt = float(row["contb_receipt_amt"])
 
 	cand["total"] += amt
-	print cand["total"]
+	# print cand["total"]
 
 	college = format_college( row["contbr_employer"] )
 	if college is not None:
-		c = cand["colleges"]
+		college_arr = cand["colleges"]
+		cand["colleges_total"] += amt 
 
-		if c.has_key(college):
-			c[college]["donators"] += 1
-			c[college]["total"] += amt
-		else:
-			c[college] = {
+		curr_c = None 
+		for c in college_arr:
+			if c["name"] == college:
+				c["donators"] += 1
+				c["total"] += amt
+				curr_c = c 
+				break
+
+		if curr_c is None:
+			new_c = {
+				"name" : college,
 				"donators" : 1,
 				"total" : amt,
-				"jobs" : {
-
-				 }
-
+				"jobs" : []
 			}
+			college_arr.append(new_c)
+			curr_c = new_c 
 
-		j = format_job( row["contbr_occupation"] )
-		if j is not None:
+		job = format_job( row["contbr_occupation"] )
+		if job is not None:
 
 			# handle jobs
-			if c[college]["jobs"].has_key(j):
-				c[college]["jobs"][j]["total"] += amt
-				c[college]["jobs"][j]["donators"] += 1
-			else:
-				c[college]["jobs"][j] = {
+			found_job = False 
+
+			for j in curr_c["jobs"]:
+				if j["title"] == job:
+					j["total"] += amt
+					j["donators"] += 1
+					found_job = True
+					break
+
+			if not found_job:
+				j = {
+					"title" : job,
 					"total" : amt,
 					"donators" : 1
 				}
+				curr_c["jobs"].append(j)
 
 
 	# handle location
-	loc = cand["locations"]
-	city = row["contbr_city"] 
-	if loc.has_key(city):
-		loc[city]["total"] += amt
-		loc[city]["donators"] += 1
-	else:
-		loc[city] = {
-			"total" : amt,
-			"donators" : 1 
-		}
+	# loc = cand["locations"]
+	# city = row["contbr_city"] 
+	# if loc.has_key(city):
+	# 	loc[city]["total"] += amt
+	# 	loc[city]["donators"] += 1
+	# else:
+	# 	loc[city] = {
+	# 		"total" : amt,
+	# 		"donators" : 1 
+	# 	}
 
 json.dump(d, jsonfile, indent=4, separators=(',', ': '))
-print(json.dumps(d))
+# print(json.dumps(d))
 
 
 
